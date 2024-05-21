8a8ba12d705dc223361b0392edd58e3468f7c9ca
tongjunshaune <41782238+tongjunshaune@users.noreply.github.com>
2024-05-21 11:17:54 +0800

    Delete FlagEmbedding directory

--- FlagEmbedding/llm_embedder/qrecc.py
+++
@@ -1,5 +0,0 @@
-import datasets
-# load dataset
-qrecc_corpus = datasets.load_dataset("namespace-Pt/qrecc-corpus", split="train")
-# save to jsonline format in YOUR data folder
-qrecc_corpus.to_json("/home/tongjun/FlagEmbedding/FlagEmbedding/llm_embedder/data/llm-embedder/convsearch/qrecc/corpus.json", force_ascii=False, lines=True, orient="records")
--- FlagEmbedding/llm_embedder/scripts/eval.sh
+++
@@ -1,1 +0,0 @@
-torchrun --nproc_per_node 1 -m evaluation.eval_popqa --query_encoder BAAI/llm-embedder --version llm-embedder --data_root /home/tongjun/FlagEmbedding/FlagEmbedding/llm_embedder/data/llm-embedder
